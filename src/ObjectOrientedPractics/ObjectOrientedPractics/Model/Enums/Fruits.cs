cda25b2ed19ec938b8d761b18dbfb406e4c27b95
Александра <atresagina@gmail.com>
2024-02-15 18:43:57 +0700

    1 feature(Fruits): добавлено перечисление

---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/Model/Enums/Fruits.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model.Enums
+{
+    public enum Fruits
+    {
+        /// <summary>
+        /// Яблоко.
+        /// </summary>
+        Apple,
+
+        /// <summary>
+        /// Персик.
+        /// </summary>
+        Peach,
+
+        /// <summary>
+        /// Апельсин.
+        /// </summary>
+        Orange,
+
+        /// <summary>
+        /// Банан.
+        /// </summary>
+        Banana
+    }
+}
