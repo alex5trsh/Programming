3d26d70b9959d79c936a3ebc42835b77e11affcf
Александра <atresagina@gmail.com>
2024-02-26 17:20:01 +0700

    верстка главного окна

---
+++ src/Contacts/View/ViewModel/LoadCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace View.ViewModel
+{
+    class LoadCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            //сохранение или загрузка контакта из файла.
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Contacts/View/ViewModel/MainVM.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using View.Model;
+
+namespace View.ViewModel
+{
+    class MainVM:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Contact currentContact;
+
+        private string _name;
+
+        /// <summary>
+        /// Номер телефона.
+        /// </summary>
+        private string _numberPhone;
+
+        /// <summary>
+        /// Почта.
+        /// </summary>
+        private string _email;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Name));
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Возвращает и задает номер. Должен состоять только из 11 положительных чисел.
+        /// </summary>
+        public string NumberPhone
+        {
+            get => _numberPhone;
+            set
+            {
+                if (value.Length != 11)
+                {
+                    throw new ArgumentException("Номер не больше 11 знаков");
+                }
+
+                if (_numberPhone != value)
+                {
+                    _numberPhone = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(NumberPhone));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Возвращает и задает почту. Должна состоять только из букв.
+        /// </summary>
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Email));
+                }
+            }
+        }
+
+    }
+
+}
